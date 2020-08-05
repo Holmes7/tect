e0382fb054cbaf942e2e875682f5f332132942b4
Holmes7 <ashutoshpatkar@yahoo.in>
2020-08-05 23:05:45 +0530

    Implemented download and info commands

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 A command line interface for audio-only download of youtube videos using the pytube module.  
 
-It is made keeping in mind my personal needs. It does not aim towards a larger audience.
+You could use this cli to download basically anything where the picture isn't really relevant like songs, debates and monologues.
+
+The project is primarily made for my personal use. It does not aim towards a larger audience.
--- tect.py
+++ tect.py
@@ -1,12 +1,34 @@
 import pytube
+import argparse
 
-url = "https://www.youtube.com/watch?v=wAtKRPX0RO4"
+parser = argparse.ArgumentParser()
+parser.add_argument("url", help="link to the video")
+parser.add_argument("--info", "-i", help='''get information on different files
+	available''', action="store_true")
+parser.add_argument("--download", "-d", help='''downloads the file from the url
+	with the provided itag''')
+args = parser.parse_args()
 
-try:
-	video_obj = pytube.YouTube(url)
-	stream = video_obj.streams.filter(only_audio=True)
-	for i in stream:
-		print(i)
+url = args.url
 
-except:
-	print("The video probably isn't downloadable")
+if args.info:
+	try:
+		vid = pytube.YouTube(url)
+		streams = vid.streams
+		for stream in streams:
+			# print(stream)
+			print(f'''itag={stream.itag} type={stream.mime_type} abr={stream.abr}''')
+
+	except Exception as e:
+		print(e)
+
+if args.download:
+	try:
+		vid = pytube.YouTube(url)
+		stream = vid.streams.get_by_itag(args.download)
+		print(stream)
+		stream.download()
+
+	except Exception as e:
+		print(e)
+
