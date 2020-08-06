d742d23d0c06d76510693bdbb058e6d270c6a534
Holmes7 <ashutoshpatkar@yahoo.in>
2020-08-07 01:28:42 +0530

    Video length bug fixed

--- tect.py
+++ tect.py
@@ -1,5 +1,7 @@
 import pytube
 import argparse
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import moviepy
 
 parser = argparse.ArgumentParser()
 parser.add_argument("url", help="link to the video")
@@ -14,6 +16,7 @@ url = args.url
 if args.info:
 	try:
 		vid = pytube.YouTube(url)
+		print(vid.title)
 		streams = vid.streams
 		for stream in streams:
 			# print(stream)
@@ -26,9 +29,14 @@ if args.download:
 	try:
 		vid = pytube.YouTube(url)
 		stream = vid.streams.get_by_itag(args.download)
-		print(stream)
-		stream.download()
+		print(f'''itag={stream.itag} type={stream.mime_type} abr={stream.abr}''')
+		# stream.download()
+		len = vid.length
+		old_mp4_file = f'{vid.title}.mp4'
+		mp4_file = f'{vid.title}1.mp4'
+		mp3_file = f'{vid.title}.mp3'
+		mp3_file = f'{vid.title}.mp3'
+		ffmpeg_extract_subclip(old_mp4_file, 0, len - 1, targetname=mp4_file)
 
 	except Exception as e:
 		print(e)
-
