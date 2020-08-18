b17b0e3bf62df0b44532a85c929945fa4196e479
Holmes7 <ashutoshpatkar@yahoo.in>
2020-08-18 20:53:43 +0530

    Annoying itunes length issue resolved

--- tect.py
+++ tect.py
@@ -3,17 +3,15 @@ import subprocess
 import os
 
 
-def change_duration(old_file, new_file, start, end, delete_old_file):
+def change_duration(old_file, new_file, start, duration, delete_old_file):
 	subprocess.run([
 		'ffmpeg',
-		'-ss',
-		start,
 		'-i',
 		old_file,
-		'-to',
-		end,
-		'-c',
-		'copy',
+		'-ss',
+		start,
+		'-t',
+		duration,
 		new_file],
 		capture_output=True)
 
@@ -29,16 +27,32 @@ def download_mp4(url, itag):
 		return True
 
 	except Exception as e:
-		return False
+		return e
 
 
 def mp4_to_mp3(mp4_file, mp3_file, delete_old_file):
+	inter_file = "inter_file.mp3"
 	subprocess.run([
 		'ffmpeg',
 		'-i',
 		mp4_file,
-		mp3_file])
+		'-f',
+		'mp3',
+		'-vn',
+		inter_file],
+		capture_output=True)
 
+	''' The second subprocess is run to solve the itunes problems. Not yet sure why this problem actually occurs as no resources avalaible but this works just fine for now'''
+	subprocess.run([
+		'ffmpeg',
+		'-i',
+		inter_file,
+		'-acodec',
+		'copy',
+		mp3_file],
+		capture_output=True)
+
+	os.remove(inter_file)
 	if delete_old_file:
 		os.remove(mp4_file)
 
@@ -48,23 +62,24 @@ def video_info(url):
 	try:
 		vid = pytube.YouTube(url)
 		streams = vid.streams
-		info_dict["title"] = vid.title
-		info_dict["streams"] = [{}]
+		# info_dict["title"] = vid.title
+		# info_dict["streams"] = [{}]
 		for stream in streams:
-			dic = {}
-			dic["itag"] = stream.itag
-			dic["type"] = stream.mime_type
-			dic["abr"] = stream.abr
-			info_dict["streams"].append(dic)
+			print(stream)
+			# dic = {}
+			# dic["itag"] = stream.itag
+			# dic["type"] = stream.mime_type
+			# dic["abr"] = stream.abr
+			# info_dict["streams"].append(dic)
 
-		return info_dict
+		# return info_dict
 
 	except Exception as e:
 		return e
 
 
-# var = video_info('https://www.youtube.com/watch?v=6su62xI2x2Q')
+# video_info('https://www.youtube.com/watch?v=6su62xI2x2Q')
 # print(var)
-# download_mp4('https://www.youtube.com/watch?v=6su62xI2x2Q', '140')
-# mp4_to_mp3('123.mp4', '143.mp3', False)
-change_duration('143.mp3', '144.mp3', '00:00:00', '00:05:00', False)
+# download_mp4('https://www.youtube.com/watch?v=cfSqTxJNLd8', '18')
+mp4_to_mp3('Madoromi.mp4', 'Madoromi1.mp3', False)
+# change_duration('Madoromi1.mp4', 'Madoromi_half.mp4', '00:00:00', '00:04:23', False)
